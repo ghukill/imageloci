789e60f91afe658d63e1fbb5e4514a4ab7396f3e
Graham Hukill <ghukill@gmail.com>
2015-09-12 12:51:20 -0400

    dive continues

---
+++ config/initializers/blacklight_initializer.rb
@@ -0,0 +1,7 @@
+# A secret token used to encrypt user_id's in the Bookmarks#export callback URL
+# functionality, for example in Refworks export of Bookmarks. In Rails 4, Blacklight
+# will use the application's secret key base instead.
+#
+
+# Blacklight.secret_key = 'ec30528610f2f2e69f0866cbd80b9a146c0427ce011f825694cff962995a3eb2aebfd5c8e6cf052a285ddb69e779980b2aa31a89176b5110bae3010819292893'
+
